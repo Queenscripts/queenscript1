54d805edd8405c976e492c2e853d54981c9dfa91
Queen <queensform@Queens-MacBook-Air.local>
2020-07-22 12:12:34 -0700

    style refactored

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/posts">Blog</Link>
+    <Link to="/blog">Blog</Link>
     <Link to="/projects">Projects</Link>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
