307000c7fb9b64fd941445f1a34077a01d48bb04
Queen <queennefertitishabazz@gmail.com>
2022-10-15 10:49:34 -0700

    testing google ads

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import Navigation from "../components/navigation";
 import 'prismjs/themes/prism-okaidia.css';
 import Img from 'gatsby-image';
 
 export default ({ children }) => {
+  useEffect(() => {
+  const script = document.createElement('script');
+
+  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8407231826487620";
+  script.async = true;
+
+  document.body.appendChild(script);
+
+  return () => {
+    document.body.removeChild(script);
+  }
+}, []);
   const data = useStaticQuery(
     graphql`
    
@@ -53,6 +65,7 @@ export default ({ children }) => {
       {children}
 
     </div>
+
       <footer className="site-footer">
         <p>&copy; {new Date().getFullYear()} Queenscript &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://queensform.com">Queensform</a></p>
         <aside>   <a class="twitter-timeline" href="https://twitter.com/_queenscript?ref_src=twsrc%5Etfw">Tweets by _queenscript</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
