40ad0ac69499dfacb6185dd23b29d6847a06085f
Queen <queennefertitishabazz@gmail.com>
2023-03-11 20:15:30 -0800

    Update layout.js - removed twitter from footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -68,7 +68,6 @@ export default ({ children }) => {
 
       <footer className="site-footer">
         <p>&copy; {new Date().getFullYear()} Queenscript &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://queensform.com">Queensform</a></p>
-        <aside>   <a class="twitter-timeline" href="https://twitter.com/_queenscript?ref_src=twsrc%5Etfw">Tweets by _queenscript</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </aside>
       </footer>
     </>
